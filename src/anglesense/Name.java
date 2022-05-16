5f26e8c4ea64cde560faedf12362430e49ce889b
HenLevi <heni8500@gmail.com>
2022-05-16 17:41:14 +0300

    Add files via upload

---
+++ src/anglesense/Name.java
@@ -0,0 +1,13 @@
+package anglesense;
+
+import java.util.List;
+
+public class Name {
+
+	public String firstName;
+	public String lastName;
+	public int[] arrayOfInt;
+	public Object[] arrayOfObj;
+	public List<String> ls;
+
+}
---
+++ src/anglesense/Person.java
@@ -0,0 +1,11 @@
+package anglesense;
+
+public class Person {
+	public int age;
+	public Name name;
+
+	public Person() {
+		name = new Name();
+	}
+
+}
